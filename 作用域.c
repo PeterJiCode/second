16213187d7348b5aee47effff55a7a9e8eefcd1e
吉永鹏 <qq2489455798@163.com>
2022-12-05 20:29:42 +0800

    提交代码

---
+++ 作用域.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//作用域和生命周期 
+int g_val=2021;
+int main()
+{
+	printf("1%d\n",g_val);
+	{
+		printf("2%d\n",g_val);
+	
+		int a =10;
+		printf("a=%d\n",a);
+	}
+	printf("3%d\n",g_val);
+	return 0;
+ } 
---
+++ 减号.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	const int num=10;
+	num = 20;
+	printf("num=%d\n",num);
+	return 0;
+ } 
